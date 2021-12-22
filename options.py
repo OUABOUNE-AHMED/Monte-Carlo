3c681914ad9bcb64519149be7195ebfc4d80a76a
OUABOUNE-AHMED <74854734+OUABOUNE-AHMED@users.noreply.github.com>
2021-12-23 00:29:36 +0100

    Update options.py

--- options.py
+++ options.py
@@ -1,1 +1,178 @@
+import streamlit as st
+import pandas as pd 
+import requests
+import investpy as py
+from bs4 import BeautifulSoup
+#import Casabourselib as cbl 
+from enum import Enum
+from datetime import datetime, timedelta
+#from MonteCarloSimulation import MonteCarloPricing
+import numpy as np
+from scipy.stats import norm 
+import matplotlib.pyplot as plt
+
+# Local package imports
+
+
+
+class MonteCarloPricing():
+    """ 
+    Class implementing calculation for European option price using Monte Carlo Simulation.
+    We simulate underlying asset price on expiry date using random stochastic process - Brownian motion.
+    For the simulation generated prices at maturity, we calculate and sum up their payoffs, average them and discount the final value.
+    That value represents option price
+    """
+
+    def __init__(self, underlying_spot_price, strike_price, days_to_maturity, risk_free_rate, sigma, number_of_simulations):
+        """
+        Initializes variables used in Black-Scholes formula .
+
+        underlying_spot_price: current stock or other underlying spot price
+        strike_price: strike price for option cotract
+        days_to_maturity: option contract maturity/exercise date
+        risk_free_rate: returns on risk-free assets (assumed to be constant until expiry date)
+        sigma: volatility of the underlying asset (standard deviation of asset's log returns)
+        number_of_simulations: number of potential random underlying price movements 
+        """
+        # Parameters for Brownian process
+        self.S_0 = underlying_spot_price
+        self.K = strike_price
+        self.T = days_to_maturity / 365
+        self.r = risk_free_rate
+        self.sigma = sigma 
+
+        # Parameters for simulation
+        self.N = number_of_simulations
+        self.num_of_steps = days_to_maturity
+        self.dt = self.T / self.num_of_steps
+
+    def simulate_prices(self):
+        """
+        Simulating price movement of underlying prices using Brownian random process.
+        Saving random results.
+        """
+        np.random.seed(20)
+        self.simulation_results = None
+
+        # Initializing price movements for simulation: rows as time index and columns as different random price movements.
+        S = np.zeros((self.num_of_steps, self.N))        
+        # Starting value for all price movements is the current spot price
+        S[0] = self.S_0
+
+        for t in range(1, self.num_of_steps):
+            # Random values to simulate Brownian motion (Gaussian distibution)
+            Z = np.random.standard_normal(self.N)
+            # Updating prices for next point in time 
+            S[t] = S[t - 1] * np.exp((self.r - 0.5 * self.sigma ** 2) * self.dt + (self.sigma * np.sqrt(self.dt) * Z))
+
+        self.simulation_results_S = S
+
+    def _calculate_call_option_price(self): 
+        """
+        Call option price calculation. Calculating payoffs for simulated prices at expiry date, summing up, averiging them and discounting.   
+        Call option payoff (it's exercised only if the price at expiry date is higher than a strike price): max(S_t - K, 0)
+        """
+        if self.simulation_results_S is None:
+            return -1
+        return np.exp(-self.r * self.T) * 1 / self.N * np.sum(np.maximum(self.simulation_results_S[-1] - self.K, 0))
+    
+
+    def _calculate_put_option_price(self): 
+        """
+        Put option price calculation. Calculating payoffs for simulated prices at expiry date, summing up, averiging them and discounting.   
+        Put option payoff (it's exercised only if the price at expiry date is lower than a strike price): max(K - S_t, 0)
+        """
+        if self.simulation_results_S is None:
+            return -1
+        return np.exp(-self.r * self.T) * 1 / self.N * np.sum(np.maximum(self.K - self.simulation_results_S[-1], 0))
+       
+
+    def plot_simulation_results(self, num_of_movements):
+        """Plots specified number of simulated price movements."""
+        plt.figure(figsize=(12,8))
+        plt.plot(self.simulation_results_S[:,0:num_of_movements])
+        plt.axhline(self.K, c='k', xmin=0, xmax=self.num_of_steps, label='Strike Price')
+        plt.xlim([0, self.num_of_steps])
+        plt.ylabel('Simulated price movements')
+        plt.xlabel('Days in future')
+        plt.title(f'First {num_of_movements}/{self.N} Random Price Movements')
+        plt.legend(loc='best')
+        plt.show()
+
+#_______________________________
+# Main title
+st.title('Pricing des Options')
+
+
+apptitle = 'Projet Pricing des options'
+
+
+# Title the app
+st.title('Valorisation des actions cotées en Bourse de Casablanca ')
+
+# @st.cache(ttl=3600, max_entries=10)   #-- Magic command to cache data
+
+# @st.cache(ttl=3600, max_entries=10)   #-- Magic command to cache data
+    
+st.sidebar.markdown("## Selectioner la methode de calcule ")
+
+
+st.markdown('### Travail realisé par: Yassine Rhzif, Ahmed Ouaboune. Filière: finance et ingenierie decisionnelle')
+#st.markdown('<center><img src="https://raw.githubusercontent.com/RHZIF/streamlit_test/main/ensa.png" width="300"  height="100" alt="Ensa logo"></center>', unsafe_allow_html=True)
+st.markdown("### Sous l'encadrement de Pr. Soukaina Hadiri")
+st.markdown('##')
+st.markdown('__________________________________________________________')
+
+
+#dropdown = st.sidebar.selectbox("Choisir une action", py.get_stocks(country='morocco').name)
+monte = st.sidebar.selectbox("methode de calcule ", ['classique','reduction variance'])
+
+#-----------------------
+
+if monte == "classique":
+  # Parameters for Monte Carlo simulation
+    ticker = st.text_input('Ticker symbol', 'AAPL')
+    strike_price = st.number_input('Strike price', 300)
+    risk_free_rate = st.slider('Risk-free rate (%)', 0, 100, 10)
+    sigma = st.slider('Sigma (%)', 0, 100, 20)
+    exercise_date = st.date_input('Exercise date', min_value=datetime.today() + timedelta(days=1), value=datetime.today() + timedelta(days=365))
+    number_of_simulations = st.slider('Number of simulations', 100, 100000, 10000)
+    num_of_movements = st.slider('Number of price movement simulations to be visualized ', 0, int(number_of_simulations/10), 100)
+
+    if st.button(f'Calculate option price for {ticker}'):
+        # Getting data for selected ticker
+        data = py.get_stock_historical_data(stock=ticker, country='United States', from_date="01/01/1900", to_date= datetime.today().strftime('%d/%m/%Y'))
+        st.write(data.tail())
+        st.line_chart(data.Close)
+
+        # Formating simulation parameters
+        spot_price = data.Close[-1] 
+        risk_free_rate = risk_free_rate / 100
+        sigma = sigma / 100
+        days_to_maturity = (exercise_date - datetime.now().date()).days
+
+        # ESimulating stock movements
+        MC = MonteCarloPricing(spot_price, strike_price, days_to_maturity, risk_free_rate, sigma, number_of_simulations)
+        MC.simulate_prices()
+
+        # Visualizing Monte Carlo Simulation
+        MC.plot_simulation_results(num_of_movements)
+        st.pyplot()
+
+        # Calculating call/put option price
+        call_option_price = MC._calculate_call_option_price()
+        put_option_price = MC._calculate_put_option_price()
+
+        # Displaying call/put option price
+        st.subheader(f'Call option price: {call_option_price}')
+        st.subheader(f'Put option price: {put_option_price}')
+
+    
+
+
+#------------------
+
+
+
+
 
