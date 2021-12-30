bff04f69762a7987caa329764b679c3d2038f301
OUABOUNE-AHMED <74854734+OUABOUNE-AHMED@users.noreply.github.com>
2021-12-31 00:57:43 +0100

    Update Pricing.py

--- Pricing.py
+++ Pricing.py
@@ -168,7 +168,17 @@ if monte == "classique":
         st.subheader(f'Call option price: {call_option_price}')
         st.subheader(f'Put option price: {put_option_price}')
 
-    else st.button(f'Calculate option price for {ticker}'):
+    else:
+        # Parameters for Monte Carlo simulation
+        #ticker = st.text_input('Ticker symbol', 'AAPL')
+        ticker = st.selectbox("Ticker symbol", py.get_stocks_list(country='United States'))
+        strike_price = st.number_input('Strike price', 100)
+        risk_free_rate = st.slider('Risk-free rate (%)', 0, 100, 10)
+        sigma = st.slider('Sigma (%)', 0, 100, 20)
+        exercise_date = st.date_input('Exercise date', min_value=datetime.today() + timedelta(days=1), value=datetime.today() + timedelta(days=365))
+        number_of_simulations = st.slider('Number of simulations', 100, 100000, 10000)
+        num_of_movements = st.slider('Number of price movement simulations to be visualized ', 0, int(number_of_simulations/10), 100)
+        st.button(f'Calculate option price for {ticker}'):
         # Getting data for selected ticker
         data = py.get_stock_historical_data(stock=ticker, country='United States', from_date="01/01/1900", to_date= datetime.today().strftime('%d/%m/%Y'))
         st.write(data.tail())
